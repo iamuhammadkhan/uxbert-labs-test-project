57418480c169a673d2bca3e5691befdc7c328e77
Muhammad Khan <muhammad.khan@aymcommerce.com>
2020-10-30 16:53:07 +0500

    Initial Commit

---
+++ UXBERT-LABS-Test-Project-MK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UXBERT-LABS-Test-Project-MK
+//
+//  Created by Muhammad Khan on 30/10/2020.
+//  Copyright © 2020 Muhammad Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
